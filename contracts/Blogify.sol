36ad3011a6c6384117ea6aa06ddb6bb14ca48864
Pawel Kowalewski <pkowalewski@microstrategy.com>
2022-07-13 16:03:47 +0200

    project config added

---
+++ contracts/Blogify.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+import "./libraries/Base64.sol";
+
+contract Blogify is ERC721 {
+
+    using SafeMath for uint64;
+    using Counters for Counters.Counter;
+    
+    constructor() ERC721("Blogify", "BLFY") {
+    }
+}
